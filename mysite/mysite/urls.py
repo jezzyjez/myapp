372a1495e7d84ad7dd250fc784f7aa1b4d596189
jezreel <zamorajezreel@gmail.com>
2018-03-08 17:01:05 +0800

    Add blog

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 
 urlpatterns = [
     path('pollss/', include('polls.url')),
+    re_path(r'', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
